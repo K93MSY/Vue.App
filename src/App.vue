3eab2a0ba5870e0b1ca649f991f3637666724dfb
K93MSY <masaya93@ogaki-tv.ne.jp>
2022-11-06 18:46:13 +0900

    Start

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <div id="a">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  padding: 30px;
+  background: #000000;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #00b7ff;
+}
+
+#a{
+  position: absolute;
+  z-index: -1;
+  top: 80px;
+  left: 0;
+  right: 0;
+  width: 100%;
+  margin: auto;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>特に🍆</h1>
+  </div>
+</template>
